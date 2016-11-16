41b9ed1dfa99d3a2f2151cee3763b0e7e254b5dc
Roman Mylenkyi <i.ua4@i.ua>
2016-11-16 18:00:49 +0000

    make changing immages and its discriptions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,83 @@
-$("")
+$("#oe").hover(function(){
+    $('#oe-img').css('opacity','1');
+},function(){
+    $('#oe-img').css('opacity','0');
+});
+$("#oe").hover(function(){
+    $('#oe-about').css('opacity','1');
+},function(){
+    $('#oe-about').css('opacity','0');
+});
+
+
+$("#skrjabin").hover(function(){
+    $('#skrjabin-img').css('opacity','1');
+},function(){
+    $('#skrjabin-img').css('opacity','0');
+});
+$("#skrjabin").hover(function(){
+    $('#skrjabin-about').css('opacity','1');
+},function(){
+    $('#skrjabin-about').css('opacity','0');
+});
+
+
+$("#tartak").hover(function(){
+    $('#tartak-img').css('opacity','1');
+},function(){
+    $('#tartak-img').css('opacity','0');
+});
+$("#tartak").hover(function(){
+    $('#tartak-about').css('opacity','1');
+},function(){
+    $('#tartak-about').css('opacity','0');
+});
+
+
+$("#skaj").hover(function(){
+    $('#skaj-img').css('opacity','1');
+},function(){
+    $('#skaj-img').css('opacity','0');
+});
+$("#skaj").hover(function(){
+    $('#skaj-about').css('opacity','1');
+},function(){
+    $('#skaj-about').css('opacity','0');
+});
+
+
+$("#bumbox").hover(function(){
+    $('#bumbox-img').css('opacity','1');
+},function(){
+    $('#bumbox-img').css('opacity','0');
+});
+$("#bumbox").hover(function(){
+    $('#bumbox-about').css('opacity','1');
+},function(){
+    $('#bumbox-about').css('opacity','0');
+});
+
+
+$("#tnmk").hover(function(){
+    $('#tnmk-img').css('opacity','1');
+},function(){
+    $('#tnmk-img').css('opacity','0');
+});
+$("#tnmk").hover(function(){
+    $('#tnmk-about').css('opacity','1');
+},function(){
+    $('#tnmk-about').css('opacity','0');
+});
+
+
+$("#pt").hover(function(){
+    $('#pt-img').css('opacity','1');
+},function(){
+    $('#pt-img').css('opacity','0');
+});
+$("#pt").hover(function(){
+    $('#pt-about').css('opacity','1');
+},function(){
+    $('#pt-about').css('opacity','0');
+});
+
